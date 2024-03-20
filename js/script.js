f523459b56ca2f0e872ee9443521872a2b255953
maksontakson <maksontukson@gmail.com>
2024-03-20 12:45:21 +0200

    Refactoring of calc part code

--- js/script.js
+++ js/script.js
@@ -322,7 +322,24 @@ window.addEventListener('DOMContentLoaded', () => {
     // calc
 
     const result = document.querySelector(".calculating__result span");
-    let sex = 'female', height, weight, age, ratio = 1.375;
+    
+    let sex = 'female',
+                 height, weight, age,
+         ratio = 1.375;
+
+    if (localStorage.getItem('sex')) {
+        sex = localStorage.getItem('sex');
+    } else {
+        sex = 'female';
+        localStorage.setItem('sex', 'female')
+    }
+
+    if (localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio');
+    } else {
+        ratio = 1.375;
+        localStorage.setItem('ratio', 1.375);
+    }
 
     function calcTotal() {
         if(!sex || !height || !weight || !age || !ratio) {
@@ -336,15 +353,30 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    calcTotal();
+
     function getStaticInformation(parentSelector, activeClass) {
         const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        elements.forEach(elem => {
+            elem.classList.remove(activeClass);
+            if(elem.getAttribute('id') === localStorage.getItem('sex')) {
+                elem.classList.add(activeClass);
+            } 
+
+            if(elem.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+                elem.classList.add(activeClass);
+            }
+        });
 
         elements.forEach(elem => {
             elem.addEventListener('click', (e) => {
                 if (e.target.getAttribute('data-ratio')) {
                     ratio = +e.target.getAttribute('data-ratio');
+                    localStorage.setItem('ratio', ratio);
                 } else {
                     sex = e.target.getAttribute('id');
+                    localStorage.setItem('sex', sex);
                 }
     
                 console.log (ratio, sex);
@@ -367,6 +399,13 @@ window.addEventListener('DOMContentLoaded', () => {
         const input = document.querySelector(selector);
 
         input.addEventListener('input', () => {
+
+            if (input.value.match(/\D/g)) {
+                input.style.border = '1px solid red';
+            } else {
+                input.style.border = 'none';
+            }
+
             switch(input.getAttribute('id')) {
                 case 'height': 
                     height = +input.value;
